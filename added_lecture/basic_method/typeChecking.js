f480d86a4d0a533748b0f3ce30b00882c953c235
wold21 <wold2180@gmail.com>
2021-03-03 11:28:51 +0900

    typeof

---
+++ added_lecture/basic_method/typeChecking.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const string = `node.js`;
+const arr = [];
+const obj = {};
+const number = 1;
+
+console.log(typeof string);
+console.log(typeof arr); // arr는 오브젝트의 영역안에 들어있다.
+console.log(typeof obj);
+console.log(typeof number);
